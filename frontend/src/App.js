c65ed15b3d8b2bb3540a552509d509ffcf01e272
Susan Choi <itsmesusanee@gmail.com>
2022-01-09 22:03:56 -0500

    navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar />
       <h1>Winner Dinner</h1>
     </div>
   );
---
+++ frontend/src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar-box">
+        <p>Recipes</p>
+        <p>About</p>
+        <p>Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
